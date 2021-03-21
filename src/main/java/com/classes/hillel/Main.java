0b98a7d42d619557c56326de7c76758cdab27b07
Maksim1988507 <makarenko.makc@gmail.com>
2021-03-21 14:56:55 +0200

    Lesson 4, learned arrays

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -4,8 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("GIT!!!!!!");
-        System.out.println("New message to github");
+        String[] array = new String[3];
+        int[] arrayInt = {1, 2, 3};
+
+        array[0] = "Hi";
+        array[1] = "dear";
+        array[2] = "friend";
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (i == arrayInt.length - 1){
+                System.out.print(arrayInt[i] + "\n");
+            }else{
+                System.out.print(arrayInt[i] + ", ");
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1){
+                System.out.print(array[i] + "\n");
+            }else{
+                System.out.print(array[i] + " ");
+            }
+        }
 
     }
 
