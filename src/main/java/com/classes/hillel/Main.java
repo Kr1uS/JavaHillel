f0019a306df7d09ba181d1f2a04d6752052a8529
Kr1uS <72558733+Kr1uS@users.noreply.github.com>
2021-04-04 14:52:50 +0300

    Added input for encryption program

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,32 +1,92 @@
 package main.java.com.classes.hillel;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
     public static void main(String[] args) {
 
-        String[] array = new String[3];
-        int[] arrayInt = {1, 2, 3};
+//        boolean c1 = true & false & true;
+//        boolean c2 = true && false && true;
 
-        array[0] = "Hi";
-        array[1] = "dear";
-        array[2] = "friend";
+        String[] alphabet = {"a", "b", "c", "d",
+                "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z"};
 
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (i == arrayInt.length - 1){
-                System.out.print(arrayInt[i] + "\n");
-            }else{
-                System.out.print(arrayInt[i] + ", ");
-            }
+        System.out.println("Press shift number, please!");
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        String text = "";
+        try {
+            text = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
-        for (int i = 0; i < array.length; i++) {
-            if (i == array.length - 1){
-                System.out.print(array[i] + "\n");
-            }else{
-                System.out.print(array[i] + " ");
-            }
+        if (text == null || text.isEmpty()) {
+            text = "0";
         }
 
+        int number = 0;
+        try {
+            number = Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Please enter text to encode:");
+
+        String encodingText = "";
+        try {
+            encodingText = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String encryptedWord = encrypt(encodingText,
+                alphabet, number);
+
+        System.out.println(encryptedWord);
     }
 
+    private static String encrypt(String word,
+                                  String[] alphabet,
+                                  int shift){
+        String encrypted = "";
+        for (int i = 0; i < word.length(); i++){
+            int index = findIndex(alphabet, word.charAt(i));
+            if (index+shift > alphabet.length-1){
+                int innerIndex = (index+shift) - alphabet.length;
+                index = innerIndex-1;
+            }
+            encrypted = encrypted.concat(alphabet[index+shift]);
+        }
+        return encrypted;
+    }
+
+    private static int findIndex(String[] alphabet,
+                                 char letter) {
+        int index = 0;
+        for (int i = 0; i<alphabet.length; i++){
+            String ch = alphabet[i];
+            if (ch.equals(String.valueOf(letter))) {
+                return i;
+            }
+        }
+        return index;
+    }
+
+    private String[] completeAlphabet(String[] alphabet) {
+        String[] completed = new String[alphabet.length * 2];
+
+        for (int i = alphabet.length; i < alphabet.length; ++i) {
+            completed[i] = alphabet[i];
+        }
+
+        return completed;
+    }
 }
