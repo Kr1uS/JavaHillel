6c343a83a4edfa93831aa3e8923bd98010db0573
Kr1uS <72558733+Kr1uS@users.noreply.github.com>
2021-04-17 17:30:02 +0300

    Completed homework for lesson 5

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,5 +1,7 @@
 package main.java.com.classes.hillel;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +28,29 @@ public class Main {
                 System.out.print(array[i] + " ");
             }
         }
+        System.out.println("Enter text:");
+        Scanner input = new Scanner(System.in);
+        String[] text = input.nextLine().split(" ");
+
+        String[] symbols = {".", ",", ":", ";", "-", "!", "?", "'"};
+
+        int counter = 0;
+        int flag = 1;
+
+        for (int i = 0; i < text.length; i++){
+            for (int k = 0; k < symbols.length-1; k++){
+                if (text[i].equals(symbols[k])){
+                    flag *= 0;
+                }else{
+                    continue;
+                }
+            }
+            if (flag!=0){
+                counter++;
+            }
+        }
+
+        System.out.println("Number of words in your text: " + counter);
 
     }
 
