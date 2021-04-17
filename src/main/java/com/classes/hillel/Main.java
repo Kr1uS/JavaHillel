2af8baf978a04f99a11571d33d794834b4577b3c
Kr1uS <72558733+Kr1uS@users.noreply.github.com>
2021-04-17 17:32:27 +0300

    Updated homework for lesson 5

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -6,28 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] array = new String[3];
-        int[] arrayInt = {1, 2, 3};
-
-        array[0] = "Hi";
-        array[1] = "dear";
-        array[2] = "friend";
-
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (i == arrayInt.length - 1){
-                System.out.print(arrayInt[i] + "\n");
-            }else{
-                System.out.print(arrayInt[i] + ", ");
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            if (i == array.length - 1){
-                System.out.print(array[i] + "\n");
-            }else{
-                System.out.print(array[i] + " ");
-            }
-        }
         System.out.println("Enter text:");
         Scanner input = new Scanner(System.in);
         String[] text = input.nextLine().split(" ");
